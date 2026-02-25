104704faa1f3eb50fccd464935dfa926de6ec7d4
Kmn-san <kumenoshouhei0612@gmail.com>
2026-02-25 12:13:18 +0800

    project setup

---
+++ public/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (<div>
+    <h1>Home Page</h1>
+    <SignedOut >
+      <SignUpButton mode="modal">Sign Up</SignUpButton>
+    </SignedOut>
+
+    <SignedIn>
+      <SignOutButton>Logout</SignOutButton>
+    </SignedIn>
+  </div>)
+}
