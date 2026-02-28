9a0835e69c33c100f7afddf98415258e61deda04
Kmn-san <kumenoshouhei0612@gmail.com>
2026-02-28 15:51:58 +0800

    tiny fixes in EditDoctorDialog.ts

--- src/lib/actions/appointments.ts
+++ src/lib/actions/appointments.ts
@@ -1,9 +1,19 @@
 "use server"
 
+import { currentUser } from "@clerk/nextjs/server"
 import { prisma } from "../prisma"
 
 export async function getAppointments() {
     try {
+        const user = await currentUser()
+        const adminEmail = process.env.ADMIN_EMAIL?.toLowerCase()
+        const userEmail = user?.emailAddresses
+            ?.map((e) => e.emailAddress.toLowerCase())
+            .find((email) => email === adminEmail)
+
+        if (!user || !adminEmail || !userEmail) {
+            throw new Error("Unauthorized")
+        }
         const appointments = await prisma.appointment.findMany({
             include: {
                 user: {
@@ -25,7 +35,7 @@ export async function getAppointments() {
 
         return appointments
     } catch (error) {
-        console.log("Error fetching appointments: ", error);
+        console.error("Error fetching appointments:", error);
         throw new Error("Failed to fetch appointments")
     }
 }
