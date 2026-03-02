c2097dadabcfefb4ad22ae22db9a3ae28e765396
Kmn-san <kumenoshouhei0612@gmail.com>
2026-03-02 16:21:29 +0800

    appointments page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import UserSync from "@/components/UserSync";
 import TanStackProvider from "@/components/providers/TanStackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,7 @@ export default function RootLayout({
           <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
             {/* this is done in the home page component */}
             <UserSync />
-            {/* <Toaster /> */}
+            <Toaster />
             {children}
           </body>
         </html>
---
+++ src/components/admin/RecentAppointments.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecentAppointments() {
+  return (
+    <div>RecentAppointments</div>
+  )
+}
+
+export default RecentAppointments
