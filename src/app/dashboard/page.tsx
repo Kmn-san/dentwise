729947d2478392feb16c9aed13e83ae56c31f8d9
Kmn-san <kumenoshouhei0612@gmail.com>
2026-03-02 14:14:31 +0800

    dashboard page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
+import ActivityOverview from '@/components/dashboard/ActivityOverview';
+import MainActions from '@/components/dashboard/MainActions';
+import WelcomeSection from '@/components/dashboard/WelcomeSection';
 import Navbar from '@/components/Navbar'
 
 const DashboardPage = () => {
     return (
-        <div>
+        <>
             <Navbar />
-            DashboardPage
-        </div>
-    )
+            <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+                <WelcomeSection />
+                <MainActions />
+                <ActivityOverview />
+            </div>
+        </>
+    );
 }
-
-export default DashboardPage
+export default DashboardPage;
